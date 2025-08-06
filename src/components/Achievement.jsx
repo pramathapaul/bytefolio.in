ed3df69a25853519e97fb8d51082947873df6308
pramathapaul <pramathapaul8090@gmail.com>
2025-08-06 13:41:31 +0530

    Add files via upload

--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -6,25 +6,25 @@ const slides = [
     id: 1,
     title: "Introduction to Data Science",
     description: "ICompleted Introduction to Data Science course from Infosys Springboard, exploring fundamentals like data analysis, visualization, and basic machine learning.",
-    image: "../assets/1-63f6813d-f268-4222-8c49-c5a5ec5ba8bc_pages-to-jpg-0001.jpg",
+    image: "/assets/1-63f6813d-f268-4222-8c49-c5a5ec5ba8bc_pages-to-jpg-0001.jpg",
   },
   {
     id: 2,
     title: "Generative AI for ALL",
     description: "Completed Generative AI for ALL course from Infosys Springboard, covering core concepts of AI and its real-world applications.",
-    image: "../assets/1-86018921-cc89-4913-90f4-566126a9bd2c_page-0001.jpg",
+    image: "/assets/1-86018921-cc89-4913-90f4-566126a9bd2c_page-0001.jpg",
   },
   {
     id: 3,
     title: "Full Stack Web Developer",
     description: "Completed Full Stack Web Developer course at Ardent Computech Pvt. Ltd., gaining hands-on experience in front-end and back-end development.",
-    image: "../assets/doc_page-0001.jpg",
+    image: "/assets/doc_page-0001.jpg",
   },
   {
     id: 4,
     title: "Programming in Java",
     description: "Completed Programming in Java course from NPTEL & IIT Kharagpur, covering core concepts of Java",
-    image: "../assets/Programming In Java_nptel_page-0001.jpg",
+    image: "/assets/Programming In Java_nptel_page-0001.jpg",
   },
 ];
 
@@ -64,4 +64,3 @@ const Achievement = () => {
 };
 
 export default Achievement;
-
